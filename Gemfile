5df4420bb86b626ab9354b25d44a2b820e381bde
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-04-19 15:52:44 -0400

    first project push

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+ruby "2.3.1"
+
+gem "sinatra"
+
+gem "activerecord"
+
+gem "sinatra-activerecord"
+
+gem "sqlite3"
+
+gem "rake"
+
+gem "sinatra-flash"
---
+++ quitter.rb
@@ -0,0 +1,8 @@
+require "sinatra"
+require "sinatra/activerecord"
+require "sinatra/flash"
+require "bundler/setup"
+require "./models"
+
+set sessions, true
+set :database, "sqlite3:quitterbase.sqlite3"
