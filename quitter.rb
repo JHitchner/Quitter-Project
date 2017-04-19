bc9c2d9258e2fd1be366f794c614a6e53ca59d8b
Juliet Hitchner <juliet.hitchner@gmail.com>
2017-04-19 17:10:10 -0400

    added profile pages

--- quitter.rb
+++ quitter.rb
@@ -4,5 +4,20 @@ require "sinatra/flash"
 require "bundler/setup"
 # require "./models"
 
-set sessions, true
+# set sessions, true
 set :database, "sqlite3:quitterbase.sqlite3"
+
+get "/profile" do
+  erb :profile
+
+end
+
+get "/profile_create" do
+  erb :profile_create
+
+end
+
+get "/profile_delete" do
+  erb :profile_delete
+
+end
