4670700f8cb9b5f834b7f4d26a085203a5ec6e0f
KHamilton177 <KHamilton177@verizon.net>
2017-04-20 11:23:17 -0400

    merging quitter.rb file

--- quitter.rb
+++ quitter.rb
@@ -2,12 +2,52 @@ require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/flash"
 require "bundler/setup"
-
-#require "./models"
+require "./models"
 
 set :database, "sqlite3:quitterbase.sqlite3"
-#set sessions: true
+set :sessions, true
 
+def current_user
+  if session[:user_id]
+    User.find(session[:user_id])
+  end
+end
+
+get "/sign-up" do
+  erb :sign_up_form
+end
+
+post "/sign-up" do
+  User.create(
+    username: params[:username],
+    password: params[:password]
+  )
+    redirect "/"
+end
+
+get "/sign-in" do
+  erb :sign_in_form
+end
+
+get "/login-fail" do
+  erb :sign_in_fail
+end
+
+post "/sign-in" do
+  @user = User.where(username: params[:username]).first
+  if @user.password == params[:password]
+    session[:user_id]=@user.id
+    flash[:notice] = "Login successful!"
+    redirect "/"
+  else
+    flash[:alert] = "Login failed."
+    redirect "/sign-in"
+  end
+end
+
+get "/sign-out" do
+  session[user_id]=nil
+end
 
 get "/profile_view" do
   erb :profile_view
