7273b1073f556056683834f673b9c9ce0ee162f3
KHamilton177 <KHamilton177@verizon.net>
2017-04-19 21:46:44 -0400

    renamed 'pwd not found', modified user related .erb files and added flash messaging

---
+++ db/migrate/20170419211210_create_additional_user_columns.rb
@@ -0,0 +1,4 @@
+class CreateAdditionalUserColumns < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
--- quitter.rb
+++ quitter.rb
@@ -4,5 +4,48 @@ require "sinatra/flash"
 require "bundler/setup"
 #require "./models"
 
-#set sessions, true
 set :database, "sqlite3:quitterbase.sqlite3"
+set :sessions, true
+
+def current_user
+  if session[:user_id]
+    User.find(session[:user_id])
+  end
+end
+
+get "/sign-up" do
+  erb :sign_up_form
+end
+
+post "/sign-up" do
+  user.create(
+    username: params[:username],
+    password: params[:password]
+)
+end
+
+get "/sign-in" do
+  erb :sign_in_form
+end
+
+get "/login-fail" do
+  erb :sign_in_fail
+end
+
+post "/sign-in" do
+  @user = User.where(username: params[:username]).id
+  if @user.password==params[:password]
+    session[:user_id]=@user.id
+      if
+        flash[:notice] = "Login successful!"
+        redirect "/"
+      else
+        flash[:alert] = "Login failed."
+    	redirect "/sign-in"
+      end
+  end
+end
+
+get "/sign-out" do
+  session[user_id]=nil
+end
