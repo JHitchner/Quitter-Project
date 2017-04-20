7798d68715041d1afe0a55c0bbb7d84d30778968
KHamilton177 <KHamilton177@verizon.net>
2017-04-19 21:57:59 -0400

    added profile_id column and user info in models.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419204300) do
+ActiveRecord::Schema.define(version: 20170419211210) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
+    t.string  "username"
+    t.string  "password"
+    t.integer "profile_id"
   end
 
 end
