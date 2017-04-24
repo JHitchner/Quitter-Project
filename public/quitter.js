68ee9014326dbcb647b30a133766cf6525fe8415
KHamilton177 <KHamilton177@verizon.net>
2017-04-23 20:37:19 -0400

    working on tooltip and user profile link

--- public/quitter.js
+++ public/quitter.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
 signUpBtn=document.querySelector("#lay_up");
+usr_prof_link=document.querySelector(".home_post_u");
+
+usr_prof_link.addEventListener("mouseover", function(){
+
+});
 
 
 
